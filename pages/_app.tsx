e808bb3a66a91dd515e62d43fcdc4235426dd43a
Junhyuk Lee <38907328+Sinclairr08@users.noreply.github.com>
2022-06-27 19:46:23 +0900

    feat: Add SWR configuration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,20 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "../components/Layout";
+import { SWRConfig } from "swr";
+import axios from "axios";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <SWRConfig
+      value={{
+        fetcher: (url: string) => axios.get(url).then((res) => res.data),
+      }}
+    >
       <Layout>
         <Component {...pageProps} />
       </Layout>
-    </>
+    </SWRConfig>
   );
 }
 
